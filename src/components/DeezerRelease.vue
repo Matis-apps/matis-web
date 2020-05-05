cc12d57c7dc1685554b421d37cdda5394dd1ed1d
Nicolas Valaise <nicolas.valaise@hotmail.fr>
2020-05-05 16:51:55 +0200

    Display the playlists with the artists

--- src/components/DeezerRelease.vue
+++ src/components/DeezerRelease.vue
@@ -14,29 +14,34 @@
           <h5 class="card-title">{{capitalize(release.content_type)}}</h5>
           <p class="card-text"><small class="text-muted">Sortie le {{ release.updated_at }}</small></p>
 
-          <ul class="list-group my-2" v-if="tracklist.length > 0">
-            <li class="list-group-item d-flex justify-content-between align-items-center"
-              v-for="track in tracklist"
-              v-bind:key="track.id">
-              {{track.artist.name}} | {{track.title}}
-              <span class="badge badge-primary badge-pill" v-show="track.readable">{{track.rank}}</span>
-            </li>
-          </ul>
-          <hr>
-          <h5 class="card-title">Related artists</h5>
-          <div class="d-flex justify-content-center row" v-if="relatedArtists.length > 0">
-            <div class="card col-2 m-3" style="width: 18rem;"
-              v-for="artist in relatedArtists"
-              v-bind:key="artist.id">
+          <div v-if="tracklist.length > 0">
+            <p><small>{{totalTracks}} tracks</small></p>
+            <ul class="list-group my-2">
+              <li class="list-group-item d-flex justify-content-between align-items-center"
+                v-for="track in tracklist"
+                v-bind:key="track.id">
+                {{track.artist.name}} | {{track.title}}
+                <span class="badge badge-primary badge-pill" v-show="track.readable">{{track.rank}}</span>
+              </li>
+            </ul>
+          </div>
+          <div v-if="release._obj == 1">
+            <hr>
+            <h5 class="card-title">Related artists</h5>
+            <div class="d-flex justify-content-center row" v-if="relatedArtists.length > 0">
+              <div class="card col-2 m-3" style="width: 18rem;"
+                v-for="artist in relatedArtists"
+                v-bind:key="artist.id">
 
-              <img class="card-img-top p-3" :src="artist.picture" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">{{artist.name}}</p>
+                <img class="card-img-top p-3" :src="artist.picture" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">{{artist.name}}</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div v-else class="alert alert-light" role="alert">
-            No artists found
+            <div v-else class="alert alert-light" role="alert">
+              No artists found
+            </div>
           </div>
         </div>
       </div>
@@ -57,6 +62,7 @@ export default {
   },
   data() {
     return {
+      totalTracks: 0,
       tracklist: [],
       relatedArtists: [],
     }
@@ -66,10 +72,20 @@ export default {
       var element = document.getElementById("deezer-release");
       element.scrollIntoView({behavior: "smooth"});
 
-      console.log(newVal);
+      this.totalTracks = 0;
+      this.tracklist = [];
+      this.relatedArtists = [];
+
       if(newVal) {
-        this.fetchTracklist(newVal.content_id);
-        this.fetchRelatedArtists(newVal.id);
+        switch(newVal._obj) {
+          case 1:
+            this.fetchAlbumTracklist(newVal.content_id);
+            this.fetchRelatedArtists(newVal.id);
+            break;
+          case 2:
+            this.fetchPlaylistTracklist(newVal.content_id); // =" . $index;
+            break;
+        }
       } 
     }
   },
@@ -78,13 +94,13 @@ export default {
       if (typeof s !== 'string') return '';
       return s.charAt(0).toUpperCase() + s.slice(1);
     },
-    async fetchTracklist (id, retry = 0) {
+    async fetchAlbumTracklist (id, retry = 0) {
       
       await this.$axios.get("http://localhost:8010/proxy/album/"+id)
         .then((response) => {
-          // console.log(response.data);
           if (response.status === 200 && response.data.tracks) {
             this.tracklist = response.data.tracks.data;
+            this.totalTracks = response.data.nb_tracks;
           } else {
             if (retry > 0) {
               // setTimeout(this.fetchTracklist, this.timeout_ms, index, retry-1);
@@ -97,9 +113,23 @@ export default {
       
       await this.$axios.get("http://localhost:8010/proxy/artist/"+id+"/related")
         .then((response) => {
-          console.log(response.data);
           if (response.status === 200 && response.data.data) {
             this.relatedArtists = response.data.data;
+          } else {
+            if (retry > 0) {
+              // setTimeout(this.fetchTracklist, this.timeout_ms, index, retry-1);
+            }
+          }
+        })
+        .catch((error) => console.log(error));
+    },
+    async fetchPlaylistTracklist (id, retry = 0) {
+      
+      await this.$axios.get("http://localhost:8010/proxy/playlist/" +id+"/tracks&limit=50&index=0")
+        .then((response) => {
+          if (response.status === 200 && response.data.data) {
+            this.tracklist = response.data.data;
+            this.totalTracks = response.data.total;
           } else {
             if (retry > 0) {
               // setTimeout(this.fetchTracklist, this.timeout_ms, index, retry-1);
--- src/views/Social.vue
+++ src/views/Social.vue
@@ -1,19 +1,19 @@
 <template>
 <div class="row">    
   <div class="social offset-1 col-3">
-    <p>{{releases.length}} artistes affichés</p>
-    <div class="progress my-2" v-show="releases.length < countArtists">
+    <p>{{releases.length}} news</p>
+    <div class="progress my-2" v-show="releases.length < countReleases">
       <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" v-bind:style="progressStyle" :aria-valuenow="progress" aria-valuemin="0" aria-valuemax="100">
         {{progressPercent}}
       </div>
     </div>
     <div v-if="releases" class="scrollbar scrollbar-primary">
-      <div class="card mb-2" v-for="(item, index) in releases" v-bind:key="item.id">
+      <div class="card mb-2" :class="{ 'border-primary' : releaseDays(item.updated_at) == 0 }" v-for="(item, index) in releases" v-bind:key="item._uid">
         <div class="card-header d-flex justify-content-between">
-          <img :src="item.picture" class="card-img-top img-fluid" alt="picture artist" style="max-height: 50px; max-width: 50px;"> {{ item.name }}
+          <img v-show="item.picture" :src="item.picture" class="card-img-top img-fluid" alt="picture artist" style="max-height: 50px; max-width: 50px;"> {{ item.name }}
         </div>
-        <div class="card-body text-left" v-if="item.content_full.length > 0">
-          <h5 class="card-title"><b>#{{index+1}}</b> Dernière sortie <small>| {{ capitalize(item.content_type) }}</small></h5>
+        <div class="card-body text-left" v-if="item.content_full">
+          <h5 class="card-title"><b>#{{index+1}}</b><small> | {{ capitalize(item.content_type) }}</small></h5>
           <p class="small" v-if="releaseDays(item.updated_at) > 0">Il y a {{ releaseDays(item.updated_at) }} jours ({{ item.updated_at }})</p>
           <p class="small" v-else-if="releaseDays(item.updated_at) < 0">Sortie prévue dans {{ Math.abs(releaseDays(item.updated_at)) }} jours ({{ item.updated_at }})</p>
           <p class="small" v-else>Sortie aujourd'hui</p>
@@ -29,7 +29,7 @@
   <div class="col-7">
     <transition name="slide-fade" >
       <DeezerRelease 
-        v-if="(releases.length >= countArtists)"
+        v-if="(releases.length >= countReleases)"
         v-bind:release="selectedRelease"/>
     </transition>
   </div>
@@ -47,17 +47,21 @@ export default {
   },
   data() {
     return {
+      user_id: 16192550,
+
       releases: [],
       timeout_ms: 1500,
       retry: 10,
+
       loadedArtists: 0,
-      countArtists: 100,
+      loadedPlaylists: 0,
+      countReleases: 0,
       selectedRelease: null
     }
   },
   computed: {
     progress: function () {
-      return Math.ceil(this.loadedArtists/this.countArtists*100);
+      return Math.ceil(this.loadedArtists/this.countReleases*100);
     },
     progressPercent: function () {
       return this.progress + "%"
@@ -68,6 +72,7 @@ export default {
   },
   created() {
     this.fetchArtists(0, this.retry);
+    this.fetchPlaylists(0, this.retry);
   },
   methods: {
     releaseDays: function (day) {
@@ -79,12 +84,18 @@ export default {
       if (typeof s !== 'string') return '';
       return s.charAt(0).toUpperCase() + s.slice(1);
     },
+
+    /**
+     * Retrieve the loved artists
+     * @params index int - For pagination
+     * @params retry int - Number of retries remaining
+     */
     async fetchArtists (index, retry) {
       
-      await this.$axios.get("http://localhost:8010/proxy/user/16192550/artists?limit=100&index="+index)
+      await this.$axios.get("http://localhost:8010/proxy/user/"+this.user_id+"/artists?limit=100&index="+index)
         .then((response) => {
           if (response.status === 200 && response.data.data) {
-            this.countArtists = response.data.total;
+            this.countReleases += response.data.total;
             response.data.data.forEach(artist => (
               artist.albums = [],
               this.fetchArtistContent(artist, this.retry)
@@ -101,6 +112,12 @@ export default {
         })
         .catch((error) => console.log(error));
     },
+
+    /**
+     * Retrieve the artists details
+     * @params artist - Response from the previous call
+     * @params retry int - Number of retries remaining
+     */
     async fetchArtistContent(artist, retry) {
       await this.$axios.get("http://localhost:8010/proxy/artist/"+artist.id+"/albums")
         .then((response) => {
@@ -118,8 +135,17 @@ export default {
         })
         .catch((error) => console.log(error));
     },
+
+    /**
+     * Retrieve the artists details
+     * @params artist - Response from the previous call
+     *
+     * Return release object
+     */
     formatArtistToFeed(artist){
       return {
+        _obj: 1,
+        _uid: artist.albums[0].record_type+'-'+artist.id+'-'+artist.albums[0].id,
         // Related to the author
         id: artist.id,
         name: artist.name,
@@ -136,8 +162,70 @@ export default {
         content_full: artist.albums,
       };
     },
+
+    /**
+     * Retrieve the loved artists
+     * @params index int - For pagination
+     * @params retry int - Number of retries remaining
+     */
+    async fetchPlaylists (index, retry) {
+      
+      await this.$axios.get("http://localhost:8010/proxy/user/"+this.user_id+"/playlists?limit=100&index="+index)
+        .then((response) => {
+          
+          if (response.status === 200 && response.data.data) {            
+            response.data.data.forEach(playlist => (
+              this.fetchPlaylistContent(playlist)
+            ));
+
+            if (response.data.next) {
+              //this.fetchPlaylists(index+100, this.retry);       
+            }
+          } else {
+            if (retry > 0) {
+              //setTimeout(this.fetchPlaylists, this.timeout_ms, index, retry-1);
+            }
+          }
+        })
+        .catch((error) => console.log(error));
+    },
+
+    /**
+     * Retrieve the artists details
+     * @params artist - Response from the previous call
+     * @params retry int - Number of retries remaining
+     */
+    fetchPlaylistContent(playlist) {
+      if(!playlist.is_loved_track && playlist.creator.id != this.user_id) {
+        this.releases.push(this.formatPlaylistToFeed(playlist)),
+        this.loadedPlaylists+=1,
+        this.countReleases += 1,
+        this.releases.sort((a,b) => this.shortLastReleases(a,b))
+      }
+    },
+
     formatPlaylistToFeed(playlist){
-      return playlist;
+      return {
+        _obj: 2,
+        _uid: playlist.type+'-'+playlist.creator.id+'-'+playlist.id,
+        // Related to the author
+        id: playlist.creator.id,
+        name: playlist.creator.name,
+        picture: null,
+        link: "https://www.deezer.com/profile/" + playlist.creator.id,
+        updated_at: playlist.time_mod ? this.timestampToDate(playlist.time_mod) : this.timestampToDate(playlist.time_add),
+        // Related to the content
+        content_id: playlist.id,
+        content_title: playlist.title,
+        content_type: playlist.type,
+        content_picture: playlist.picture_big,
+        content_link: playlist.link,
+        content_last: playlist,
+        content_full: playlist,
+      };
+    },
+    timestampToDate(seconds) {
+      return this.$moment.unix(seconds).format("YYYY-MM-DD");
     },
     clickOnRelease(release) {
       this.selectedRelease = release;
