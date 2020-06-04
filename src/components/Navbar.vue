eb33bc8b1364a247f5151507d1c942ed81f8b9ae
Nicolas Valaise <nicolas.valaise@hotmail.fr>
2020-06-04 19:50:44 +0200

    Close navbar from the tabs only if the navbar expanded

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-default navbar-light bg-white navbar-expand-md fixed-top">
-    <router-link class="navbar-brand" to="/" data-toggle="collapse" data-target="#navbarNavDropdown" exact>Matis</router-link>
+    <router-link class="navbar-brand" to="/" data-toggle="collapse" data-target="#navbarNavDropdown.show" exact>Matis</router-link>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -8,19 +8,19 @@
     <div v-if="isAuthenticated" class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-          <router-link class="nav-link" to="/account" data-toggle="collapse" data-target="#navbarNavDropdown" exact-path>Account</router-link>
+          <router-link class="nav-link" to="/account" data-toggle="collapse" data-target="#navbarNavDropdown.show" exact-path>Account</router-link>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" to="/releases" data-toggle="collapse" data-target="#navbarNavDropdown" exact>Releases</router-link>
+          <router-link class="nav-link" to="/releases" data-toggle="collapse" data-target="#navbarNavDropdown.show" exact>Releases</router-link>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" to="/playlists" data-toggle="collapse" data-target="#navbarNavDropdown" exact>Playlists</router-link>
+          <router-link class="nav-link" to="/playlists" data-toggle="collapse" data-target="#navbarNavDropdown.show" exact>Playlists</router-link>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" to="/social" data-toggle="collapse" data-target="#navbarNavDropdown" exact>Social</router-link>
+          <router-link class="nav-link" to="/social" data-toggle="collapse" data-target="#navbarNavDropdown.show" exact>Social</router-link>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" to="/search" data-toggle="collapse" data-target="#navbarNavDropdown" exact>Search</router-link>
+          <router-link class="nav-link" to="/search" data-toggle="collapse" data-target="#navbarNavDropdown.show" exact>Search</router-link>
         </li>
       </ul>
       <NavbarSelectPlatform
@@ -29,15 +29,15 @@
     <div v-else class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-          <router-link class="nav-link" to="/login" data-toggle="collapse" data-target="#navbarNavDropdown" exact>Login</router-link>
+          <router-link class="nav-link" to="/login" data-toggle="collapse" data-target="#navbarNavDropdown.show" exact>Login</router-link>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" to="/register" data-toggle="collapse" data-target="#navbarNavDropdown" exact>Register</router-link>
+          <router-link class="nav-link" to="/register" data-toggle="collapse" data-target="#navbarNavDropdown.show" exact>Register</router-link>
         </li>
       </ul>
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
-          <router-link class="nav-link" to="/get-started" data-toggle="collapse" data-target="#navbarNavDropdown" exact>Get Started</router-link>
+          <router-link class="nav-link" to="/get-started" data-toggle="collapse" data-target="#navbarNavDropdown.show" exact>Get Started</router-link>
         </li>
       </ul>
     </div>
