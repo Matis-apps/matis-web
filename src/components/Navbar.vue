9bc068825eb3abefa9107ccb95ab24455eeaca36
Nicolas Valaise <nicolas.valaise@hotmail.fr>
2020-06-24 00:15:48 +0200

    Creating the Matis's logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,9 @@
 <template>
   <nav class="navbar navbar-default navbar-light bg-white navbar-expand-md fixed-top">
-    <router-link class="navbar-brand" to="/" data-toggle="collapse" data-target="#navbarNavDropdown.show" exact>Matis</router-link>
+    <router-link class="navbar-brand" to="/" data-toggle="collapse" data-target="#navbarNavDropdown.show" exact>
+      <img src="/icon.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+      Matis
+    </router-link>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -66,7 +69,7 @@ export default {
     };
   },
   methods: {
-    
+
   },
 };
 </script>
